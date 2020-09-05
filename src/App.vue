f6622436c1e97b1c433846f80242718a462b44c7
seongwans <sonson1023@gmail.com>
2020-09-05 17:55:44 +0200

    title 변경

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
     <section class="hero is-primary is-primary is-bold">
       <div class="hero-body">
         <div class="container">
-          <h1 class="title">!!!</h1>
+          <h1 class="title">자동으로 빈칸 만들기</h1>
         </div>
       </div>
     </section>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     configureWebpack: {
         devtool: 'source-map'
+    },
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            title: '빈칸으로 공부해요'
+        }
     }
 }
